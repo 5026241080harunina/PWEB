28eb2e7d0200a75b5c828551ef1da8ff447278e1
Harunina <haruninairene06@gmail.com>
2026-05-25 11:47:35 +0700

    pertemuan 12

---
+++ 5026241080a-main/routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DosenController;
+use App\Http\Controllers\PegawaiDBController;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('halo', function () {
+	return "<h1> Halo, Selamat datang</h1> di tutorial laravel www.malasngoding.com";
+});
+
+Route::get('blog', function () {
+	return view('blog');
+});
+
+Route::get('pert5', function () {
+	return view('pertemuan5');
+});
+
+Route::get('dosen', [DosenController::class, 'index']);
+Route::get('biodata', [DosenController::class, 'biodata']);
+
+//route CRUD
+Route::get('/pegawai', [PegawaiDBController::class, 'index']);
+Route::get('/pegawai/tambah', [PegawaiDBController::class, 'tambah']);
+Route::post('/pegawai/store', [PegawaiDBController::class, 'store']);
+Route::get('/pegawai/edit/{id}', [PegawaiDBController::class, 'edit']);
+Route::post('/pegawai/update', [PegawaiDBController::class, 'update']);
+Route::get('/pegawai/hapus/{id}', [PegawaiDBController::class, 'hapus']);
+Route::get('/pegawai/cari', [PegawaiDBController::class, 'cari']);
+
